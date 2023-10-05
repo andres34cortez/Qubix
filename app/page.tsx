9e5a22ad46b3620e381f9cb491ae5acd12aebacf
andres34cortez <andrescel34@gmail.com>
2023-10-05 13:25:20 -0300

    libreria de diseños agregada

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-import Image from 'next/image'
+import { Button } from "@/components/ui/button"
+
+
 
 export default function Home() {
   return (
-   <div>aca empieza la pagina</div>
+    <div>
+      <Button>Click me</Button>
+    </div>
   )
 }
