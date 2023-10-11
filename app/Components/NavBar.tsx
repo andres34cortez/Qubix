dbc588892cda08fa1bebc36f0629ef06b47ca686
andres34cortez <andrescel34@gmail.com>
2023-10-11 14:39:23 -0300

    fixed navbar

--- app/Components/NavBar.tsx
+++ app/Components/NavBar.tsx
@@ -5,10 +5,10 @@ import Image from "next/image";
 import LogoColor from "../Assets/LogoColor.svg";
 
 export default function Navbar() {
-  const [navHidden, setNavHidden] = useState(false);
-  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  /*   const [navHidden, setNavHidden] = useState(false);
+  const [prevScrollPos, setPrevScrollPos] = useState(0); */
 
-  useEffect(() => {
+  /*   useEffect(() => {
     const handleScroll = () => {
       const currentScrollPos = window.scrollY;
 
@@ -26,7 +26,7 @@ export default function Navbar() {
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, [prevScrollPos]);
+  }, [prevScrollPos]); */
 
   const scrollTo = (id: string) => {
     const servicesSection = document.getElementById(id);
@@ -37,18 +37,16 @@ export default function Navbar() {
 
   return (
     <nav
-      className={`fixed flex flex-row justify-between z-10 py-3 px-16 w-full border-b border-[#E5E7EB] shadow-[1px_-3px_50px_5px_rgba(230,230,230,0.6)] transition-transform duration-300 bg-white ${
-        navHidden ? "transform -translate-y-full opacity-0" : ""
-      }`}
+      className={` flex fixed flex-row justify-between z-10 py-3 px-16 w-full border-b border-[#E5E7EB] shadow-[1px_-3px_50px_5px_rgba(230,230,230,0.6)] transition-transform duration-300 bg-white `}
     >
-      <Link href="/">
-        <Image src={LogoColor} alt="Logo" className="w-[110px]" />
+      <Link href='/'>
+        <Image src={LogoColor} alt='Logo' className='w-[110px]' />
       </Link>
-      <ul className="flex space-x-6 items-center">
+      <ul className='flex space-x-6 items-center'>
         <li>
           <h1
             onClick={() => scrollTo("Services")}
-            className="text-[#303030] hover:text-black hover:font-medium duration-300"
+            className='text-[#303030] hover:text-black hover:font-medium duration-300'
           >
             Servicios
           </h1>
@@ -56,7 +54,7 @@ export default function Navbar() {
         <li>
           <h1
             onClick={() => scrollTo("Contact")}
-            className="text-[#303030] hover:text-black hover:font-medium duration-300"
+            className='text-[#303030] hover:text-black hover:font-medium duration-300'
           >
             Contacto
           </h1>
