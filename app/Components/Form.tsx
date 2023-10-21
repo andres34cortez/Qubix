59470830b974c41b0ea6bebd2a2911b4a3d26f3b
andres34cortez <andrescel34@gmail.com>
2023-10-21 17:41:40 -0300

    logos y fotos

--- app/Components/Form.tsx
+++ app/Components/Form.tsx
@@ -53,67 +53,67 @@ export function ContactForm() {
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className="space-y-8 max-w-[800px] w-full mx-auto border border-[#E5E7EB] rounded-[6px] p-4 bg-gray-100"
+        className='space-y-8 max-w-[800px] w-full mx-auto border border-[#E5E7EB] rounded-[6px] p-4 bg-gray-100'
         /*   className='space-y-8 w-[600px] self-center border border-[#E5E7EB] rounded-[6px] p-9 bg-gray-100' */
       >
-        <div className="flex flex-row items-center justify-">
+        <div className='flex flex-row items-center justify-'>
           <FormField
             control={form.control}
-            name="name"
+            name='name'
             render={({ field }) => (
-              <FormItem className="pr-4 flex-1">
-                <FormLabel className="text-xl text-[#544b54]">Nombre</FormLabel>
+              <FormItem className='pr-4 flex-1'>
+                <FormLabel className='text-xl text-[#544b54]'>Nombre</FormLabel>
                 <FormControl>
-                  <Input placeholder="Nombre" {...field} />
+                  <Input placeholder='Nombre' {...field} />
                 </FormControl>
-                <FormMessage className="text-sm" />
+                <FormMessage className='text-sm' />
               </FormItem>
             )}
           />
           <FormField
             control={form.control}
-            name="email"
+            name='email'
             render={({ field }) => (
-              <FormItem className="pr-4 flex-1">
-                <FormLabel className="text-xl text-[#544b54]">Email</FormLabel>
+              <FormItem className='pr-4 flex-1'>
+                <FormLabel className='text-xl text-[#544b54]'>Email</FormLabel>
                 <FormControl>
-                  <Input placeholder="Email" {...field} />
+                  <Input placeholder='Email' {...field} />
                 </FormControl>
-                <FormMessage className="text-sm" />
+                <FormMessage className='text-sm' />
               </FormItem>
             )}
           />
           <FormField
             control={form.control}
-            name="phone"
+            name='phone'
             render={({ field }) => (
-              <FormItem className="flex-1">
-                <FormLabel className="text-xl text-[#544b54]">
+              <FormItem className='flex-1'>
+                <FormLabel className='text-xl text-[#544b54]'>
                   Telefono
                 </FormLabel>
                 <FormControl>
-                  <Input placeholder="Telefono" {...field} />
+                  <Input placeholder='Telefono' {...field} />
                 </FormControl>
-                <FormMessage className="text-sm" />
+                <FormMessage className='text-sm' />
               </FormItem>
             )}
           />
         </div>
         <FormField
           control={form.control}
-          name="message"
+          name='message'
           render={({ field }) => (
             <FormItem>
-              <FormLabel className="text-xl text-[#544b54]">Mensaje</FormLabel>
+              <FormLabel className='text-xl text-[#544b54]'>Mensaje</FormLabel>
               <FormControl>
-                <Textarea placeholder="Dejanos tu mensaje!" {...field} />
+                <Textarea placeholder='Dejanos tu mensaje!' {...field} />
               </FormControl>
-              <FormMessage className="text-sm" />
+              <FormMessage className='text-sm' />
             </FormItem>
           )}
         />
-        <div className="flex flex-col sm:flex-row justify-end items-center">
-          <Button type="submit">Enviar</Button>
+        <div className='flex flex-col sm:flex-row justify-end items-center'>
+          <Button type='submit'>Enviar</Button>
         </div>
       </form>
     </Form>
--- app/Components/Marquee.tsx
+++ app/Components/Marquee.tsx
@@ -2,11 +2,13 @@
 import Image from "next/image";
 import React from "react";
 import arcor from "../Assets/Clients/Arcor_logo.svg.png";
-import Cat from "../Assets/Clients/finning.svg";
-import SaintG from "../Assets/Clients/saintG.svg";
-import YPF from "../Assets/Clients/ypfluz.svg";
-import Distrocuyo from "../Assets/Clients/distrocuyo.svg";
-import Mseguridad from "../Assets/Clients/maxiseguridad.svg";
+import Cat from "../Assets/Clients/finning.png";
+import SaintG from "../Assets/Clients/saintG.png";
+import YPF from "../Assets/Clients/ypfluz.png";
+import Distrocuyo from "../Assets/Clients/distroCuyo.jpg";
+import Mseguridad from "../Assets/Clients/maxiseguridad.png";
+import PedidosYa from "../Assets/Clients/pedidosYa.jpg";
+import JoseMaria from "../Assets/Clients/joseMaria.png";
 /* la lista de los clientes:
 Finning Caterpillar 
 Saint Gobain
@@ -26,6 +28,8 @@ const Marquee: React.FC = () => {
         <Image className="w-20 mr-12" src={YPF} alt="" />
         <Image className="w-20 mr-12" src={Distrocuyo} alt="" />
         <Image className="w-20 mr-12" src={Mseguridad} alt="" />
+        <Image className="w-20 mr-12" src={PedidosYa} alt="" />
+        <Image className="w-20 mr-12" src={JoseMaria} alt="" />
         {/* repeat */}
         <Image className="w-20 mr-12" src={arcor} alt="" />
         <Image className="w-20 mr-12" src={Cat} alt="" />
@@ -33,60 +37,80 @@ const Marquee: React.FC = () => {
         <Image className="w-20 mr-12" src={YPF} alt="" />
         <Image className="w-20 mr-12" src={Distrocuyo} alt="" />
         <Image className="w-20 mr-12" src={Mseguridad} alt="" />
+        <Image className="w-20 mr-12" src={PedidosYa} alt="" />
+        <Image className="w-20 mr-12" src={JoseMaria} alt="" />
         <Image className="w-20 mr-12" src={arcor} alt="" />
         <Image className="w-20 mr-12" src={Cat} alt="" />
         <Image className="w-20 mr-12" src={SaintG} alt="" />
         <Image className="w-20 mr-12" src={YPF} alt="" />
         <Image className="w-20 mr-12" src={Distrocuyo} alt="" />
         <Image className="w-20 mr-12" src={Mseguridad} alt="" />
+        <Image className="w-20 mr-12" src={PedidosYa} alt="" />
+        <Image className="w-20 mr-12" src={JoseMaria} alt="" />
         <Image className="w-20 mr-12" src={arcor} alt="" />
         <Image className="w-20 mr-12" src={Cat} alt="" />
         <Image className="w-20 mr-12" src={SaintG} alt="" />
         <Image className="w-20 mr-12" src={YPF} alt="" />
         <Image className="w-20 mr-12" src={Distrocuyo} alt="" />
         <Image className="w-20 mr-12" src={Mseguridad} alt="" />
+        <Image className="w-20 mr-12" src={PedidosYa} alt="" />
+        <Image className="w-20 mr-12" src={JoseMaria} alt="" />
         <Image className="w-20 mr-12" src={arcor} alt="" />
         <Image className="w-20 mr-12" src={Cat} alt="" />
         <Image className="w-20 mr-12" src={SaintG} alt="" />
         <Image className="w-20 mr-12" src={YPF} alt="" />
         <Image className="w-20 mr-12" src={Distrocuyo} alt="" />
         <Image className="w-20 mr-12" src={Mseguridad} alt="" />
+        <Image className="w-20 mr-12" src={PedidosYa} alt="" />
+        <Image className="w-20 mr-12" src={JoseMaria} alt="" />
         <Image className="w-20 mr-12" src={arcor} alt="" />
         <Image className="w-20 mr-12" src={Cat} alt="" />
         <Image className="w-20 mr-12" src={SaintG} alt="" />
         <Image className="w-20 mr-12" src={YPF} alt="" />
         <Image className="w-20 mr-12" src={Distrocuyo} alt="" />
         <Image className="w-20 mr-12" src={Mseguridad} alt="" />
+        <Image className="w-20 mr-12" src={PedidosYa} alt="" />
+        <Image className="w-20 mr-12" src={JoseMaria} alt="" />
         <Image className="w-20 mr-12" src={arcor} alt="" />
         <Image className="w-20 mr-12" src={Cat} alt="" />
         <Image className="w-20 mr-12" src={SaintG} alt="" />
         <Image className="w-20 mr-12" src={YPF} alt="" />
         <Image className="w-20 mr-12" src={Distrocuyo} alt="" />
         <Image className="w-20 mr-12" src={Mseguridad} alt="" />
+        <Image className="w-20 mr-12" src={PedidosYa} alt="" />
+        <Image className="w-20 mr-12" src={JoseMaria} alt="" />
         <Image className="w-20 mr-12" src={arcor} alt="" />
         <Image className="w-20 mr-12" src={Cat} alt="" />
         <Image className="w-20 mr-12" src={SaintG} alt="" />
         <Image className="w-20 mr-12" src={YPF} alt="" />
         <Image className="w-20 mr-12" src={Distrocuyo} alt="" />
         <Image className="w-20 mr-12" src={Mseguridad} alt="" />
+        <Image className="w-20 mr-12" src={PedidosYa} alt="" />
+        <Image className="w-20 mr-12" src={JoseMaria} alt="" />
         <Image className="w-20 mr-12" src={arcor} alt="" />
         <Image className="w-20 mr-12" src={Cat} alt="" />
         <Image className="w-20 mr-12" src={SaintG} alt="" />
         <Image className="w-20 mr-12" src={YPF} alt="" />
         <Image className="w-20 mr-12" src={Distrocuyo} alt="" />
         <Image className="w-20 mr-12" src={Mseguridad} alt="" />
+        <Image className="w-20 mr-12" src={PedidosYa} alt="" />
+        <Image className="w-20 mr-12" src={JoseMaria} alt="" />
         <Image className="w-20 mr-12" src={arcor} alt="" />
         <Image className="w-20 mr-12" src={Cat} alt="" />
         <Image className="w-20 mr-12" src={SaintG} alt="" />
         <Image className="w-20 mr-12" src={YPF} alt="" />
         <Image className="w-20 mr-12" src={Distrocuyo} alt="" />
         <Image className="w-20 mr-12" src={Mseguridad} alt="" />
+        <Image className="w-20 mr-12" src={PedidosYa} alt="" />
+        <Image className="w-20 mr-12" src={JoseMaria} alt="" />
         <Image className="w-20 mr-12" src={arcor} alt="" />
         <Image className="w-20 mr-12" src={Cat} alt="" />
         <Image className="w-20 mr-12" src={SaintG} alt="" />
         <Image className="w-20 mr-12" src={YPF} alt="" />
         <Image className="w-20 mr-12" src={Distrocuyo} alt="" />
         <Image className="w-20 mr-12" src={Mseguridad} alt="" />
+        <Image className="w-20 mr-12" src={PedidosYa} alt="" />
+        <Image className="w-20 mr-12" src={JoseMaria} alt="" />
       </div>
     </div>
   );
